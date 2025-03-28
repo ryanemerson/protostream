6e13fb757c2b4677155a854ba17538599d411491
Ryan Emerson <remerson@redhat.com>
2025-03-28 10:37:38 +0000

    Final

--- core/src/main/java/org/infinispan/protostream/impl/StringUtil.java
+++ core/src/main/java/org/infinispan/protostream/impl/StringUtil.java
@@ -7,74 +7,148 @@ import java.lang.invoke.LambdaMetafactory;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
 import java.util.function.Function;
 import java.util.function.ToIntFunction;
 
-public class StringUtil {
+import sun.misc.Unsafe;
+
+class StringUtil {
+
+   interface Utf8Helper {
+      boolean providesLatin1Bytes();
+      byte[] getBytes(String s);
+   }
 
    private static final Log log = Log.LogFactory.getLog(ProtoStreamReaderImpl.class);
    private static final Byte LATIN1 = (byte) 0;
 
-   static ToIntFunction<String> STRING_CODER;
-   static Function<String, byte[]> STRING_VALUE;
+   static volatile Utf8Helper INSTANCE;
 
-   static {
-      try {
-         MethodHandles.Lookup lookup = JDKUtils.trustedLookup(String.class);
-         MethodHandle coder = lookup.findSpecial(
-               String.class,
-               "coder",
-               methodType(byte.class),
-               String.class
-         );
-         CallSite applyAsInt = LambdaMetafactory.metafactory(
-               lookup,
-               "applyAsInt",
-               methodType(ToIntFunction.class),
-               methodType(int.class, Object.class),
-               coder,
-               MethodType.methodType(byte.class, String.class)
-         );
-         STRING_CODER = (ToIntFunction<String>) applyAsInt.getTarget().invokeExact();
+   static Utf8Helper getUtf8Helper() {
+      if (INSTANCE == null) {
+         // Attempt to use IMPL_LOOKUP so that we utilise a Lambda - Most efficient
+         try {
+            INSTANCE = createLambdaHelper();
+            return INSTANCE;
+         } catch (Throwable t) {
+            log.warnf(t, "Unable to retrieve MethodHandles required for Lambda based String optimisations");
+         }
+         // Fallback to MethodHandler - Slightly less efficient and requires  --add-opens java.base/java.lang=ALL-UNNAMED
 
-         MethodHandle value = lookup.findSpecial(
-               String.class,
-               "value",
-               methodType(byte[].class),
-               String.class
-         );
-         CallSite apply = LambdaMetafactory.metafactory(
-               lookup,
-               "apply",
-               methodType(Function.class),
-               methodType(Object.class, Object.class),
-               value,
-               methodType(byte[].class, String.class)
-         );
-         STRING_VALUE = (Function<String, byte[]>) apply.getTarget().invokeExact();
+         try {
+            INSTANCE = createFieldHelper();
+            return INSTANCE;
+         } catch (Throwable t) {
+            log.warnf(t, "Unable to retrieve MethodHandles required for field based String optimisations");
+         }
+         // Coder optimisations not possible
+         INSTANCE = new Utf8Helper() {
+            @Override
+            public boolean providesLatin1Bytes() {
+               return false;
+            }
 
-      } catch (Throwable t) {
-         log.warn("Could not retrieve MethodHandles for String.class, ignoring coder optimisations:", t);
-         STRING_CODER = null;
-         STRING_VALUE = null;
+            @Override
+            public byte[] getBytes(String s) {
+               return s.getBytes(StandardCharsets.UTF_8);
+            }
+         };
       }
+      return INSTANCE;
    }
 
-   private StringUtil() {
+   private static Utf8Helper createLambdaHelper() throws Throwable {
+      Class<MethodHandles.Lookup> lookupClass = MethodHandles.Lookup.class;
+      Field implLookup = lookupClass.getDeclaredField("IMPL_LOOKUP");
+
+      Field theUnsafeField = Unsafe.class.getDeclaredField("theUnsafe");
+      theUnsafeField.setAccessible(true);
+      Unsafe unsafe = (Unsafe) theUnsafeField.get(null);
+      long fieldOffset = unsafe.staticFieldOffset(implLookup);
+      MethodHandles.Lookup trustedLookup = (MethodHandles.Lookup) unsafe.getObject(lookupClass, fieldOffset);
+      MethodHandle constructor = trustedLookup.findConstructor(
+            MethodHandles.Lookup.class,
+            methodType(void.class, Class.class, Class.class, int.class)
+      );
+
+      MethodHandles.Lookup lookup = (MethodHandles.Lookup) constructor.invoke(String.class, null, -1);
+      MethodHandle coder = lookup.findSpecial(
+            String.class,
+            "coder",
+            methodType(byte.class),
+            String.class
+      );
+      CallSite applyAsInt = LambdaMetafactory.metafactory(
+            lookup,
+            "applyAsInt",
+            methodType(ToIntFunction.class),
+            methodType(int.class, Object.class),
+            coder,
+            MethodType.methodType(byte.class, String.class)
+      );
+      ToIntFunction<String> coderFn = (ToIntFunction<String>) applyAsInt.getTarget().invokeExact();
+
+      MethodHandle value = lookup.findSpecial(
+            String.class,
+            "value",
+            methodType(byte[].class),
+            String.class
+      );
+      CallSite apply = LambdaMetafactory.metafactory(
+            lookup,
+            "apply",
+            methodType(Function.class),
+            methodType(Object.class, Object.class),
+            value,
+            methodType(byte[].class, String.class)
+      );
+      Function<String, byte[]> valueFn = (Function<String, byte[]>) apply.getTarget().invokeExact();
+
+      return new Utf8Helper() {
+         @Override
+         public boolean providesLatin1Bytes() {
+            return true;
+         }
+
+         @Override
+         public byte[] getBytes(String s) {
+            if (coderFn.applyAsInt(s) != LATIN1)
+               return s.getBytes(StandardCharsets.UTF_8);
+
+            return valueFn.apply(s);
+         }
+      };
    }
 
-   public static boolean isUTF8CoderOptimisationSupported() {
-      return STRING_CODER != null;
-   }
+   private static Utf8Helper createFieldHelper() throws Throwable {
+      Method privateMethod = String.class.getDeclaredMethod("coder");
+      privateMethod.setAccessible(true);
+      MethodHandle STRING_CODER_HANDLER = MethodHandles.lookup().unreflect(privateMethod);
 
-   private static boolean isLatin1(String s) {
-      return STRING_CODER != null && STRING_CODER.applyAsInt(s) == LATIN1;
-   }
+      privateMethod = String.class.getDeclaredMethod("value");
+      privateMethod.setAccessible(true);
+      MethodHandle STRING_VALUE_HANDLER = MethodHandles.lookup().unreflect(privateMethod);
 
-   public static byte[] getBytes(String s) {
-      if (!isLatin1(s))
-         return s.getBytes(StandardCharsets.UTF_8);
-      return STRING_VALUE.apply(s);
+      return new Utf8Helper() {
+         @Override
+         public boolean providesLatin1Bytes() {
+            return true;
+         }
+
+         @Override
+         public byte[] getBytes(String s) {
+            try {
+               if (STRING_CODER_HANDLER.invoke(s) != LATIN1)
+                  return s.getBytes(StandardCharsets.UTF_8);
+
+               return (byte[]) STRING_VALUE_HANDLER.invoke(s);
+            } catch (Throwable t) {
+               throw new RuntimeException(t);
+            }
+         }
+      };
    }
 }
--- core/src/main/java/org/infinispan/protostream/impl/TagWriterImpl.java
+++ core/src/main/java/org/infinispan/protostream/impl/TagWriterImpl.java
@@ -27,8 +27,6 @@ import org.infinispan.protostream.descriptors.WireType;
  */
 public final class TagWriterImpl implements TagWriter, ProtobufTagMarshaller.WriteContext {
 
-   public static boolean CODER_OPTIMISATION = false;
-
    private static final Log log = Log.LogFactory.getLog(TagWriterImpl.class);
 
    private final SerializationContextImpl serCtx;
@@ -311,6 +309,8 @@ public final class TagWriterImpl implements TagWriter, ProtobufTagMarshaller.Wri
 
    //todo [anistor] need to provide a safety mechanism to limit message size in bytes and message nesting depth on write ops
    private abstract static class Encoder {
+
+      protected final StringUtil.Utf8Helper utf8Helper = StringUtil.getUtf8Helper();
       /**
        * Commits the witten bytes after several write operations were performed. Updates counters, positions, whatever.
        */
@@ -372,7 +372,7 @@ public final class TagWriterImpl implements TagWriter, ProtobufTagMarshaller.Wri
       }
 
       void writeUTF8Field(int fieldNumber, String value) throws IOException {
-         byte[] utf8buffer = StringUtil.getBytes(value);
+         byte[] utf8buffer = utf8Helper.getBytes(value);
          writeLengthDelimitedField(fieldNumber, utf8buffer.length);
          writeBytes(utf8buffer, 0, utf8buffer.length);
       }
@@ -1141,11 +1141,9 @@ public final class TagWriterImpl implements TagWriter, ProtobufTagMarshaller.Wri
    private static class OutputStreamRandomAccessEncoder extends Encoder {
 
       final RandomAccessOutputStream out;
-      final boolean useCoderOptimisation;
 
       public OutputStreamRandomAccessEncoder(RandomAccessOutputStream out) {
          this.out = out;
-         this.useCoderOptimisation = CODER_OPTIMISATION && StringUtil.isUTF8CoderOptimisationSupported();
       }
 
       @Override
@@ -1300,14 +1298,14 @@ public final class TagWriterImpl implements TagWriter, ProtobufTagMarshaller.Wri
 
       @Override
       void writeUTF8Field(int number, String s) throws IOException {
-         if (useCoderOptimisation)
-            writeUTF8FieldCoder(number, s);
+         if (utf8Helper.providesLatin1Bytes())
+            writeUTF8FieldRawBytes(number, s);
          else
-            writeUTF8FieldRewrite(number, s);
+            writeUTF8FieldWithRewind(number, s);
       }
 
-      void writeUTF8FieldCoder(int number, String s) throws IOException {
-         byte[] bytes = StringUtil.getBytes(s);
+      void writeUTF8FieldRawBytes(int number, String s) throws IOException {
+         byte[] bytes = utf8Helper.getBytes(s);
          int startPos = out.getPosition();
          out.ensureCapacity(startPos + MAX_INT_VARINT_SIZE + MAX_INT_VARINT_SIZE + bytes.length);
          startPos = writeVarInt32Direct(startPos, WireType.makeTag(number, WireType.WIRETYPE_LENGTH_DELIMITED));
@@ -1316,7 +1314,7 @@ public final class TagWriterImpl implements TagWriter, ProtobufTagMarshaller.Wri
          out.setPosition(startPos + bytes.length);
       }
 
-      void writeUTF8FieldRewrite(int number, String s) throws IOException {
+      void writeUTF8FieldWithRewind(int number, String s) throws IOException {
          int strlen = s.length();
          int tag = WireType.makeTag(number, WireType.WIRETYPE_LENGTH_DELIMITED);
          int startPos = out.getPosition();
